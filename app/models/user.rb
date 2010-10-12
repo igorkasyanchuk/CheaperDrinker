ddae8dd742bc81de78e4a7f6c2b4ba6266a1f252
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2010-10-12 12:21:41 -0400

    location

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     config.crypto_provider = Authlogic::CryptoProviders::BCrypt
   end
 
-  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :admin
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :admin, :location
 
   validates_presence_of :email
   validates_presence_of :first_name

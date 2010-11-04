22af21238fe5b2f228d871d43329bdb463e4a528
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2010-11-04 16:59:02 -0400

    password reset

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,10 +1,10 @@
 class Notifications < ActionMailer::Base
-  default_url_options[:host] = 'b2m.heroku.com'
-  default :from => 'B2M <mail@b2m.com>'
+  default_url_options[:host] = 'happyhourscout.com'
+  default :from => 'HappyHourScout.com <noreply@happyhourscout.com>'
 
   def forgot_password(user)
     @edit_password_reset_url = edit_password_reset_url(user.perishable_token)
-    mail :to => user.email
+    mail :to => user.email, :subject => "Forgot Password"
   end
 
 end

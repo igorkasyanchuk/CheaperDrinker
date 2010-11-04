502f93cc8b709e086be6ea0b04d7a8b41276fc20
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2010-11-04 07:19:50 -0400

    removed warnings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,3 +47,5 @@ RailsjazzCom::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
+
+Sass::Plugin.options[:never_update] = true

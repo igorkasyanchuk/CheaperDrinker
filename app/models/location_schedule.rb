e1e9d52893725199a667954b878ada366d8f3547
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2010-11-21 17:28:37 -0500

    added hoptoad

--- app/models/location_schedule.rb
+++ app/models/location_schedule.rb
@@ -3,6 +3,7 @@ class LocationSchedule < ActiveRecord::Base
   FIELDS = [:starts_monday_at, :ends_monday_at, :starts_tuesday_at, :ends_tuesday_at, :starts_wednesday_at, :ends_wednesday_at,
       :starts_thursday_at, :ends_thursday_at, :starts_friday_at, :ends_friday_at, :starts_saturday_at, :ends_saturday_at, :starts_sunday_at, :ends_sunday_at]
 
+
   belongs_to :location
   
   def has_shedule?
